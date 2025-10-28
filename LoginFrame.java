eecdbdf23ac612817e41aeac0c15522d74339948
manickamsandeep0-code <manickamsandeep0@gmail.com>
2025-10-29 00:13:52 +0530

    Update LoginFrame.java

--- LoginFrame.java
+++ LoginFrame.java
@@ -173,9 +173,17 @@ public class LoginFrame extends JFrame {
     }
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
         } catch (Exception e) {
-            e.printStackTrace();
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ex) {
+            }
         }
         SwingUtilities.invokeLater(() -> {
             new LoginFrame();
